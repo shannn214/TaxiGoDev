1780332c5ba619f7fc81dc531b53be30353725d8
Grace <gracejin214@gmail.com>
2018-09-05 14:33:37 +0800

    add model struct

---
+++ Driver.swift
@@ -0,0 +1,8 @@
+//
+//  Driver.swift
+//  Pods-TaxiGoDev_Example
+//
+//  Created by 尚靖 on 2018/9/5.
+//
+
+import Foundation
--- Example/TaxiGoDev/LoginViewController.swift
+++ Example/TaxiGoDev/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TaxiGoDev
 
 class LoginViewController: UISearchController {
 
