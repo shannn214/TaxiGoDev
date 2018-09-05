d19528af80a36bb598adfad024b4578b378adb10
Grace <gracejin214@gmail.com>
2018-09-05 14:38:04 +0800

    add ride model

--- Driver.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Driver.swift
-//  Pods-TaxiGoDev_Example
-//
-//  Created by 尚靖 on 2018/9/5.
-//
-
-import Foundation
---
+++ TaxiGoDev/Classes/Ride.swift
@@ -0,0 +1,99 @@
+//
+//  Ride.swift
+//  Pods-TaxiGoDev_Example
+//
+//  Created by 尚靖 on 2018/9/5.
+//
+
+import Foundation
+
+extension TaxiGo.API {
+    
+    struct Ride: Codable {
+        
+        var id: String?
+        
+        var startLatitude: Double?
+        
+        var startLongitude: Double?
+        
+        var startAddress: String?
+        
+        var endLatitude: Double?
+        
+        var endLongitude: Double?
+        
+        var endAddress: String?
+        
+        var requestTime: Double?
+        
+        var status: String?
+        
+        var driver: Driver?
+        
+    }
+    
+    struct Driver {
+        
+        var driverId: Double?
+        
+        var driverLatitude: Double?
+        
+        var driverLongitude: Double?
+        
+        var eta: Double?
+        
+        var name: String?
+        
+        var plateNumber: String?
+        
+        var vehicle: String?
+        
+    }
+    
+    struct Rider {
+        
+        var name: String?
+        
+        var profileImg: String?
+        
+        var favorite: [Favorite]?
+        
+    }
+    
+    struct Favorite {
+        
+        var address: String?
+        
+        var lat: Double?
+        
+        var lng: Double?
+        
+    }
+    
+    struct NearbyDrivers {
+        
+        var lat: Double?
+        
+        var lng: Double?
+        
+    }
+    
+    struct RequestRideLocation {
+        
+        var startLatitude: Double?
+        
+        var startLongitude: Double?
+        
+        var startAddress: String?
+        
+        var endLatitude: Double?
+        
+        var endLongitude: Double?
+        
+        var endAddress: String?
+        
+    }
+
+    
+}
